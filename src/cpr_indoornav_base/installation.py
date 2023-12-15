adecf09dae471871bb94670d6d498488830ba6ee
José Mastrangelo <jmastrangelo@clearpathrobotics.com>
2023-12-15 12:29:23 -0500

    added the platform adaptor launch file to the system job

--- src/cpr_indoornav_base/installation.py
+++ src/cpr_indoornav_base/installation.py
@@ -64,6 +64,7 @@ def create_indoornav_service(platform, local_user="administrator", remote_user="
         launch_files = [
             f'{platform}_indoornav.launch',   # will be renamed! see below
             'safety_stop.launch',
+            'platform_adaptor.launch',
             'power_monitor.launch',
             'indoornav_imu.launch',
             'indoornav_wireless.launch',
